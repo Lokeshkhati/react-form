daf97d8b0d08691f1a9b17871cb0cb091acebdd0
Lokesh Khati <62974339+Lokeshkhati@users.noreply.github.com>
2022-01-21 11:27:54 +0530

    form done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,70 @@
 import "./styles.css";
+import { useState } from "react";
+const App = () => {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    isSignedUP: true
+  });
 
-export default function App() {
+  function handleChange(event) {
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        [event.target.name]: event.target.value
+      };
+    });
+  }
+  function handleSubmit() {}
   return (
     <div className="App">
-      <h1>Sign up page</h1>
-      
+      <form>
+        <input
+          className="form-input"
+          name="firstName"
+          type="text"
+          placeholder="First name"
+        />
+        <input
+          className="form-input"
+          name="lastName"
+          type="text"
+          placeholder="Last name"
+        />
+        <input
+          className="form-input"
+          name="email"
+          type="email"
+          placeholder="Email"
+        />
+        <input
+          className="form-input"
+          name="password"
+          type="password"
+          placeholder="password"
+        />
+        <input
+          className="form-input"
+          name="confirmPassword"
+          type="password"
+          placeholder="Confirm password"
+        />
+        <div className="checkbox">
+          <input
+            className="input-checkbox"
+            name="isSignedUp"
+            type="checkbox"
+            placeholder="Confirm password"
+          />
+          <label> I Agree to the terms & conditons</label>
+        </div>
+        <button className="signUp-btn">Sign Up</button>
+      </form>
     </div>
   );
-}
+};
+
+export default App;
---
+++ src/components/signUp.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const signUp = () => {
+  return (
+    <div>
+      <h1>Sign up page</h1>
+      <h1>hello this is lokesh</h1>
+    </div>
+  );
+};
+
+export default signUp;
